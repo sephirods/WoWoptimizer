26976715255145442a86f8342e873c51c6554086
sephirods <99566331+sephirods@users.noreply.github.com>
2026-09-20 07:29:23 -0400

    chore(news): update pinned highlights [3 articles]

--- js/data/pinned_news.js
+++ js/data/pinned_news.js
@@ -2,8 +2,9 @@
 // Este archivo es público y se lee en index.html y admin.html
 window.WOW_PINNED_NEWS_CONFIG = {
   "pinnedIds": [
-    "blizz-30111968",
-    "blizz-24302093"
-  ],
-  "lastUpdated": "2026-09-20T11:25:00.000Z"
+  "blizz-24301508",
+  "blizz-30111968",
+  "blizz-24302093"
+],
+  "lastUpdated": "2026-09-20T11:29:21.059Z"
 };
